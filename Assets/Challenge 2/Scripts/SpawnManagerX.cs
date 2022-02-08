d9e9a935774ca4f6d0b96a60506505d56d22f4a8
Steven Kennedy <umbrylax@gmail.com>
2022-02-08 14:53:15 -0700

    Fixed Ball spawning

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -20,13 +20,14 @@ public class SpawnManagerX : MonoBehaviour
     }
 
     // Spawn random ball at random x position at top of play area
-    void SpawnRandomBall ()
+    void SpawnRandomBall()
     {
+        int spawnIndex = Random.Range(0, ballPrefabs.Length);
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[spawnIndex], spawnPos, ballPrefabs[spawnIndex].transform.rotation);
     }
 
 }
