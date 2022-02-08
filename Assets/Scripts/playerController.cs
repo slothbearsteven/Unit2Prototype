1775292c1d82dd805570088799258108e5ef66e7
Steven Kennedy <umbrylax@gmail.com>
2022-02-08 11:07:57 -0700

    adding projectile assigment to player

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     public float horizontalInput;
+    public GameObject projectilePrefab;
     public float speed = 10.0f;
     public float xRange = 10;
     // Start is called before the first frame update
